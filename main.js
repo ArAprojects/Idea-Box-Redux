9d6622260344642fa8a3d29648f1d43609a59b3f
Alek <alekaker@outlook.com>
2019-04-03 08:49:02 -0600

    adding ability to delete idea-cards

---
+++ main.js
@@ -0,0 +1,15 @@
+// ---------------Query-selectors--------------------//
+// var card = document.querySelector(".card")
+var deleteButton = document.querySelector(".delete-card-button");
+var bottomDisplay = document.querySelector(".bottom-display");
+
+// ---------------Event-listeners---------------------//
+bottomDisplay.addEventListener('click', deleteCard);
+
+
+//-----------------Functions-------------------//
+  function deleteCard(e) {
+  if (e.target.className === "delete-card-button") {
+    e.target.closest(".card").remove();
+  }
+};
