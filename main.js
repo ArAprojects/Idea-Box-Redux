521ad3f4cf90e41ed8958885e19bb7fcee03ec04
Jon O'Drobinak <jodrobinak@gmail.com>
2019-04-03 18:22:56 -0600

    add save card button behavior

--- main.js
+++ main.js
@@ -4,8 +4,15 @@
 var deleteButton = document.querySelector(".delete-card-button");
 var bottomDisplay = document.querySelector(".bottom-display");
 
+var cardTitleInput = document.querySelector("#card-title-input");
+var cardBodyInput = document.querySelector("#text-body-input");
+var saveIdeaButton = document.querySelector(".save-button");
+
 // ---------------Event-listeners---------------------//
 bottomDisplay.addEventListener('click', deleteCard);
+cardTitleInput.addEventListener('keyup', checkCardInputs);
+cardBodyInput.addEventListener('keyup', checkCardInputs);
+// window.addEventListener('load', setup(), true);
 
 
 //-----------------Functions-------------------//
@@ -14,6 +21,26 @@ bottomDisplay.addEventListener('click', deleteCard);
     e.target.closest(".card").remove();
   }
 };
+ 
+// //Function to set button status on page load
+// function setup() {
+//   saveIdeaButton.disabled = true;
+// }
+
+function checkCardInputs () {
+  var TitleInput = cardTitleInput.value;
+  var BodyInput = cardBodyInput.value;
+
+  if (TitleInput === "" || BodyInput === "") {
+    saveIdeaButton.disabled = true;
+    console.log('Save button disabled');
+  } else {
+    saveIdeaButton.disabled = false;
+    console.log('Save Button sb lit!');
+  }
+
+
+}
 
 
 var objectArrayOfIdeas = [];
