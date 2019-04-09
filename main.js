6303a09dd1a2319fe064534d0bc347c9d0385d7e
Brandy Mello <brandyamello@gmail.com>
2019-04-09 07:51:30 -0600

    Work on persistance in test files

--- main.js
+++ main.js
@@ -115,8 +115,9 @@ blankCard.classList.remove('displayEmpty');
 //   }
 // }
 
-// function updateCardQuality() {
-
+// function updateCardQuality(e) {
+    // var cardQuality = document.querySelector("card-quality");
+    // cardQuality.innerText = cardArray.quality.value;
 // }
 
 
