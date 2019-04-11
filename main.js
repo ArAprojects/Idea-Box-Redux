2327830014703e0256f085e643ea2030c3bd9830
Brandy Mello <brandyamello@gmail.com>
2019-04-10 22:24:49 -0600

    Clean up documents

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-// ---------------Query-selectors-------  -------------------//
 var deleteButton = document.querySelector(".delete-card-button");
 var bottomDisplay = document.querySelector(".bottom-display");
 var cardTitleInput = document.querySelector("#card-title-input");
@@ -8,13 +6,10 @@ var saveButton = document.querySelector(".save-button");
 var hidden = document.querySelector(".hidden-menu")
 var menu = document.querySelector(".menu")
 var searchBox = document.querySelector("#search-input");
-let idea = new Idea ();
+let idea = new Idea();
 var blankCard = document.querySelector('.blank-card');
 var cardArray = [];
 var cardArrayIndx = 0;
-
-// ---------------Event-listeners---------------------------//
-
 
 window.addEventListener('load', setup(), true);
 bottomDisplay.addEventListener('click', deleteCard);
@@ -27,33 +22,29 @@ cardBodyInput.addEventListener('keyup', checkCardInputs);
 menu.addEventListener('click', toggleMenu);
 searchBox.addEventListener('keyup', searchRealtime);
 
-//-----------------Functions-------------------------------//
-
- function saveCardInfo(e){
-   var cardID = Date.now();
-   var cardTitle = cardTitleInput.value;
-   var cardBody = cardBodyInput.value;
+function saveCardInfo(e) {
+  var cardID = Date.now();
+  var cardTitle = cardTitleInput.value;
+  var cardBody = cardBodyInput.value;
   idea = new Idea(cardID, cardTitleInput.value, cardBodyInput.value, false, quality[0]);
   cardArray[cardArrayIndx] = idea;
   createIdeaCard(idea);
   idea.saveToStorage(cardArray, cardArrayIndx);
- // console.log('cardArray after save2Storge: ' + cardArray);
   cardArrayIndx++;
 }
 
-//----------------clearing-inputs-------------//
-function resetInputs(){
-    cardBodyInput.value = "";
-    cardTitleInput.value = "";
+function resetInputs() {
+  cardBodyInput.value = "";
+  cardTitleInput.value = "";
 }
 
 function createIdeaCard(create) {
-  // blankCard.classList.add('displayEmpty');
+  blankCard.classList.add('displayEmpty');
   bottomDisplay.innerHTML = `
   <aside class="card" data-cardIdentifier="${create.id}">
     <div class="card-title">
       <img class="star-card-button" src="assets/star.svg" alt="">
-        <img class="delete-card-button" src="assets/delete.svg" alt="">
+      <img class="delete-card-button" src="assets/delete.svg" alt="">
     </div>
     <div class="card-body-title">
       <h3 class="idea-title" contenteditable="true">${create.title}</h3>
@@ -64,68 +55,60 @@ function createIdeaCard(create) {
       <h4>Quality: <span class="quality-level-selection">Swill</span></h4>
       <img class="down-quality-button" src="assets/downvote.svg" alt="">
     </div>
-  </aside>
-
-  ` + bottomDisplay.innerHTML;
+  </aside>` 
+  + bottomDisplay.innerHTML;
 }
 
-//-----------------delete-card----------------//
-  function deleteCard(e) {
+function deleteCard(e) {
   if (e.target.className === "delete-card-button") {
-  console.log(cardArray.length)
-    var card = e.target.closest(".card");
-    var rtrndArray = idea.deleteFromStorage(card.dataset.cardidentifier);
-  //  console.log('cardArray after deleteCard function: ' + cardArray);
-    e.target.closest(".card").remove();
-
+  var card = e.target.closest(".card");
+  var rtrndArray = idea.deleteFromStorage(card.dataset.cardidentifier);
+  e.target.closest(".card").remove();
   }
-};
+}
 
-//-----------activate-status-icons-------------//
 bottomDisplay.addEventListener('mouseover', e => {
   if(e.target.classList.contains('delete-card-button')) {
   e.target.closest(".delete-card-button").setAttribute('src', 'assets/delete-active.svg');
   }
-})
+});
 
 bottomDisplay.addEventListener('mouseout', e => {
   if(e.target.classList.contains('delete-card-button')) {
   e.target.closest(".delete-card-button").setAttribute('src', 'assets/delete.svg');
   }
-})
+});
 
 bottomDisplay.addEventListener('mouseover', e => {
   if(e.target.classList.contains('up-quality-button')) {
   e.target.closest(".up-quality-button").setAttribute('src', 'assets/upvote-active.svg');
   }
-})
+});
 
 bottomDisplay.addEventListener('mouseout', e => {
   if(e.target.classList.contains('up-quality-button')) {
   e.target.closest(".up-quality-button").setAttribute('src', 'assets/upvote.svg');
   }
-})
+});
 
 bottomDisplay.addEventListener('mouseover', e => {
   if(e.target.classList.contains('down-quality-button')) {
   e.target.closest(".down-quality-button").setAttribute('src', 'assets/downvote-active.svg');
   }
-})
+});
 
 bottomDisplay.addEventListener('mouseout', e => {
   if(e.target.classList.contains('down-quality-button')) {
   e.target.closest(".down-quality-button").setAttribute('src', 'assets/downvote.svg');
   }
-})
+});
 
 bottomDisplay.addEventListener('click', e => {
   e.target.closest(".star-card-button").getAttribute('src') === 'assets/star.svg' ?
   e.target.closest(".star-card-button").setAttribute('src', 'assets/star-active.svg') :
   e.target.closest(".star-card-button").setAttribute('src', 'assets/star.svg')
-})
+});
 
-
-//-------------top-right-input-validation-------------//
 function checkCardInputs() {
   var titleInput = cardTitleInput.value;
   var bodyInput = cardBodyInput.value;
@@ -136,63 +119,54 @@ function checkCardInputs() {
   }
 }
 
+function toggleMenu() {
+  hidden.classList.toggle("hidden-menu")
+}
 
-// ------------burger-menu--------------------------/
-  function toggleMenu(){
-    hidden.classList.toggle("hidden-menu")
-  }
-
-//---------------------Adding / Deleting Blank Card from Bottom Display -----------//
-  function displayBlankCard() {
-    if (cardArray.length > 1) {
-    blankCard.classList.remove('.displayEmpty');
-    console.log(cardArray.length + "hi")
+function displayBlankCard() {
+  if (cardArray.length < 1) {
+    blankCard.classList.remove('displayEmpty');
   }
 }
 
-//------------------Swill, Plausible, Genius Up/Down functionality-------------//
- function findId(e) {
-   var currentCard = e.target.closest(".card");
-   var currentId = parseInt(currentCard.getAttribute('data-cardIdentifier'))
-   var ideaLocation = cardArray.findIndex(i => i.id === currentId)
-   console.log(ideaLocation);
-   return ideaLocation
+function findId(e) {
+  var currentCard = e.target.closest(".card");
+  var currentId = parseInt(currentCard.getAttribute('data-cardIdentifier'));
+  var ideaLocation = cardArray.findIndex(i => i.id === currentId);
+  return ideaLocation;
  }
 
 function updateCard(e) {
   if (e.target.className === 'up-quality-button') {
-   var targetedCard = e.target.closest(".card");
-   ideaLocation = findId(e);
-   cardArray[ideaLocation].upVote()
-   var qualityText = e.target.parentNode.childNodes[3].childNodes[1];
-   qualityText.innerText = cardArray[ideaLocation].quality;
+    var targetedCard = e.target.closest(".card");
+    ideaLocation = findId(e);
+    cardArray[ideaLocation].qualityUp();
+    var qualityText = e.target.parentNode.childNodes[3].childNodes[1];
+    qualityText.innerText = cardArray[ideaLocation].quality;
   }
   if(e.target.className === 'down-quality-button') {
-   var targetedCard = e.target.closest(".card");
-   ideaLocation = findId(e);
-   cardArray[ideaLocation].downVote()
-   var qualityText = e.target.parentNode.childNodes[3].childNodes[1];
-   qualityText.innerText = cardArray[ideaLocation].quality;
-    ;
+    var targetedCard = e.target.closest(".card");
+    ideaLocation = findId(e);
+    cardArray[ideaLocation].qualityDown();
+    var qualityText = e.target.parentNode.childNodes[3].childNodes[1];
+    qualityText.innerText = cardArray[ideaLocation].quality;
   }
 }
 
-function searchRealtime(subStrInput){
+function searchRealtime(subStrInput) {
   var subString = searchBox.value;
   var searchArray = cardArray;
-  console.log('Card array is: ' + cardArray);
-  console.log('Search input value = ' + subString);
 }
+
 function setup() {
-  if(localStorage.getItem('cardArray')){
+  if(localStorage.getItem('cardArray')) {
     var getCardArray = localStorage.getItem('cardArray');
     var currentCardsInfo = JSON.parse(getCardArray);
     currentCardsInfo.forEach(function(idea){
-      createIdeaCard(idea);
-      // cardArray.push(idea);
-      idea = new Idea(idea.id, idea.title, idea.body);
-      cardArray[cardArrayIndx] = idea;
-      cardArrayIndx++;
+    createIdeaCard(idea);
+    idea = new Idea(idea.id, idea.title, idea.body);
+    cardArray[cardArrayIndx] = idea;
+    cardArrayIndx++;
     });
   }
 }
